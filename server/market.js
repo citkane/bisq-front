c194d799720eb0f820bb31f663b4f63ad695627d
Michael Jonker <michael@openpoint.ie>
2018-01-18 18:02:58 +0000

    Added license headers

--- server/market.js
+++ server/market.js
@@ -1,4 +1,24 @@
 "use strict"
+
+/*
+ * Copyright 2018 Michael Jonker (http://openpoint.ie)
+ *
+ * This file is part of bisq-front.
+ *
+ * bisq-front is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * bisq-front is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with bisq-front. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const request = require('request');
 const tools = require('./tools.js');
 
